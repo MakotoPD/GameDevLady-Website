e72931b82597ee9d631402c895a5cfa36253bed5
MakotoPL <48807218+MakotoPD@users.noreply.github.com>
2022-11-16 17:21:22 +0100

    edit error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,8 +1,14 @@
 <template>
 	<div>
-	  <h1 v-if="error.statusCode === 404">Page not found</h1>
-	  <h1 v-else>An error occurred</h1>
-	  <NuxtLink to="/">Home page</NuxtLink>
+		<Menu/>
+		<div class="py-36 flex justify-center items-center flex-col">
+			<div class="mb-12">
+				<h1 v-if="error.statusCode === 404" class="text-4xl textPrim">Page not found</h1>
+				<h1 v-else class="text-4xl textPrim">An error occurred</h1>
+			</div>
+			<NuxtLink to="/" class="px-6 py-1 rounded-xl bg-red-400 hover:bg-red-300">Home page</NuxtLink>
+		</div>
+		
 	</div>
   </template>
   
